fd11e6260c1539a39b2c9648f3d7520eb9365907
Brian <ndanumumo93@gmail.com>
2025-09-11 22:13:46 +0300

    mumo

--- packages/bot-web-ui/src/pages/dptool/dptool.tsx
+++ packages/bot-web-ui/src/pages/dptool/dptool.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import './dptool.scss';
+import './dptool.module.scss';
 
 const DPTool = () => {
     const [selectedMarket, setSelectedMarket] = useState('');
